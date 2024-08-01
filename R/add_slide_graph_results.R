d2371fb492f4246263bfe6654a6217dc00cb0566
JeffJonesTDOC <jeff.jones@teladochealth.com>
2024-08-01 15:42:14 -0500

    -Modified add_slide_graph_results:
    Renamed textBoxFT to roi_calculation_table.
    Modified the .Rd helper function to reflect this change.

--- R/add_slide_graph_results.R
+++ R/add_slide_graph_results.R
@@ -1,4 +1,4 @@
-add_slide_graph_results <- function(ppt, post_period_length, format1 = centurygothic24purple, format2 = centurygothic8gray, graphs, did_table, program, supply_cost, price_of_program, text_box, roi_table) {
+add_slide_graph_results <- function(ppt, post_period_length, format1 = centurygothic24purple, format2 = centurygothic8gray, graphs, did_table, program, supply_cost, price_of_program, roi_calculation_table, roi_table) {
   if (post_period_length==1) {
     ppt <- officer::add_slide(x=ppt, layout = "Blank Layout", master = "Livongo Slide Template 2020 Q3")
     ppt <- officer::ph_with(x=ppt,value=officer::fpar(officer::ftext(paste("Total Spending -",study,program,"ROI"),format1)),location = officer::ph_location_type(type="title")) #format1
@@ -9,7 +9,7 @@ add_slide_graph_results <- function(ppt, post_period_length, format1 = centurygo
     } else if (program == "Hypertension") {
       ppt <- officer::ph_with(x=ppt, value = officer::fpar(officer::ftext(paste("$",price_of_program,": Livongo PPPM fee",sep=""),format2)), location = officer::ph_location(left=8.3,top=5.1,width=1.5,height=0.3))
     }
-    ppt <- officer::ph_with(x=ppt,value=textBoxFT,location = officer::ph_location(left=0.25,top=3.2,width=4.15,height=0.6))
+    ppt <- officer::ph_with(x=ppt,value=roi_calculation_table,location = officer::ph_location(left=0.25,top=3.2,width=4.15,height=0.6))
   } else {
     ppt <- officer::add_slide(x=ppt, layout = "Blank Layout", master = "Livongo Slide Template 2020 Q3")
     ppt <- officer::ph_with(x=ppt,value=officer::fpar(officer::ftext(paste("Total Spending -",study,program,"ROI"),format1)),location = officer::ph_location_type(type="title")) #format1
