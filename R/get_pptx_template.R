5dca1dfbb545f18cf5b21ed9939f0c6085e53716
JeffJonesTDOC <jeff.jones@teladochealth.com>
2024-08-12 13:06:35 -0500

    -Fixed an oversight with get_pptx_template where it was not changing back to the previous session's working directory.
    -Modified the example code to point to the correct template directory in get_pptx_template.

--- R/get_pptx_template.R
+++ R/get_pptx_template.R
@@ -1,6 +1,8 @@
 get_pptx_template <- function(pptx_dir,template_name) {
+  current_dir = getwd()
   require(officer)
   setwd(pptx_dir) # Your local directory that contains the PowerPoint template.
   ppt <- officer::read_pptx(template_name)
+  setwd(current_dir)
   return(ppt)
 }
